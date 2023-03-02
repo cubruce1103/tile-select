bad0604dad235de0de30eb2a93609815a3bf007a
CUBrucE <47922426+CUBrucE@users.noreply.github.com>
2023-03-02 13:18:43 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+var maxDistance, minDistance;
+var maxDistancePad, minDistancePad;
+var randomAngle, randomDistance; 
+var x, y;
+
+//placeholder definition
+maxDistance = 150;
+minDistance = 70;
+
+maxDistancePad = maxDistance + 1
+minDistancePad = Math.max(0, minDistance - 1)
+
+//angle distance method
+var remainingAreaRatio = (minDistancePad / maxDistancePad) ** 2;
+
+do{
+    randomAngle = Math.random() * Math.PI * 2
+    randomDistance = maxDistancePad * Math.sqrt(1 - (1 - remainingAreaRatio) * Math.random())
+
+    x = Math.round(Math.cos(randomAngle) * randomDistance)
+    y = Math.round(Math.sin(randomAngle) * randomDistance)
+} while (x**2 + y**2 < minDistance**2 || x**2 + y**2 > maxDistance**2);
+
+console.log("Next tile : [" + x + "/" + y + "]");
