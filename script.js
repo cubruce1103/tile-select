30b7dd412c970808d41411b12ef5e17a98ca9910
CUBrucE <47922426+CUBrucE@users.noreply.github.com>
2023-03-03 16:24:27 +0000

    .

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-var maxDistance, minDistance;
-var maxDistancePad, minDistancePad;
-var randomAngle, randomDistance; 
-var x, y;
+function generateTile(){
+    var maxDistance, minDistance;
+    var maxDistancePad, minDistancePad;
+    var randomAngle, randomDistance; 
+    var x, y;
+    
+    minDistance = +document.getElementById("minRadius").value;
+    maxDistance = +document.getElementById("maxRadius").value;
 
-//placeholder definition
-maxDistance = 150;
-minDistance = 70;
+    if(maxDistance < minDistance){
+        [maxDistance, minDistance] = [minDistance, maxDistance];
+    }
 
-maxDistancePad = maxDistance + 1
-minDistancePad = Math.max(0, minDistance - 1)
+    maxDistancePad = maxDistance + 1
+    minDistancePad = Math.max(0, minDistance - 1)
 
-//angle distance method
-var remainingAreaRatio = (minDistancePad / maxDistancePad) ** 2;
+    //angle distance method
+    var remainingAreaRatio = (minDistancePad / maxDistancePad) ** 2;
 
-do{
-    randomAngle = Math.random() * Math.PI * 2
-    randomDistance = maxDistancePad * Math.sqrt(1 - (1 - remainingAreaRatio) * Math.random())
+    do{
+        randomAngle = Math.random() * Math.PI * 2
+        randomDistance = maxDistancePad * Math.sqrt(1 - (1 - remainingAreaRatio) * Math.random())
 
-    x = Math.round(Math.cos(randomAngle) * randomDistance)
-    y = Math.round(Math.sin(randomAngle) * randomDistance)
-} while (x**2 + y**2 < minDistance**2 || x**2 + y**2 > maxDistance**2);
-
-console.log("Next tile : [" + x + "/" + y + "]");
+        x = Math.round(Math.cos(randomAngle) * randomDistance)
+        y = Math.round(Math.sin(randomAngle) * randomDistance)
+    } while (x**2 + y**2 < minDistance**2 || x**2 + y**2 > maxDistance**2);
+    
+    document.getElementById("result").innerHTML = "Generated Tile: [" + x + "/" + y + "]";
+}
